3a0f24c5e5714fa6e410d7072478d0a7dc749786
DylanAshton2206 <Dylinger2002@gmail.com>
2022-01-26 20:35:31 -0500

    Initial Commit

---
+++ DylanLab11/src/main/java/Dylan/Ashton/n01442206/DylanActivity.java
@@ -0,0 +1,14 @@
+package Dylan.Ashton.n01442206;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DylanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
