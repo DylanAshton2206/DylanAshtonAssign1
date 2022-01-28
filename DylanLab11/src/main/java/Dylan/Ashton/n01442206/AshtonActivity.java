a2a8f82c78c439b25644733a453200a172240c25
DylanAshton2206 <Dylinger2002@gmail.com>
2022-01-28 14:59:44 -0500

    Added important comments~ Added button 1 and 2~ Added text field~ changed background of second page to light yellow,made welcome message~ changed activty_main to activity_dylan~ linked youtube and toast to button 1~ linked AshtonActivity to button 2~

---
+++ DylanLab11/src/main/java/Dylan/Ashton/n01442206/AshtonActivity.java
@@ -0,0 +1,15 @@
+package Dylan.Ashton.n01442206;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Dylan Ashton n01442206
+public class AshtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ashton);
+    }
+}
--- DylanLab11/src/main/java/Dylan/Ashton/n01442206/DylanActivity.java
+++ DylanLab11/src/main/java/Dylan/Ashton/n01442206/DylanActivity.java
@@ -1,14 +1,53 @@
 package Dylan.Ashton.n01442206;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+//Dylan Ashton n01442206 RNA
 public class DylanActivity extends AppCompatActivity {
+    private Button DylanButton1;
+    private Button DylanButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dylan);
+
+        DylanButton1 = (Button) findViewById(R.id.DylanButton1);
+        DylanButton2 = (Button) findViewById(R.id.DylanButton2);
+        DylanButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenNewScreen();
+            }
+        });
+        DylanButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast();
+                goToWebPage("https://www.youtube.com/");
+            }
+        });
+    }
+
+    public void OpenNewScreen() {
+        Intent intent = new Intent(this, AshtonActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayToast() {
+        Toast.makeText(DylanActivity.this, "Dylan Ashton , youtube", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToWebPage(String yourUrl) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(yourUrl));
+        startActivity(intent);
     }
 }
