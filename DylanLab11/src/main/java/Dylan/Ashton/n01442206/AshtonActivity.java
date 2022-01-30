1079a3345cf954254ce484abb0ce5d8412319248
DylanAshton2206 <Dylinger2002@gmail.com>
2022-01-29 20:06:51 -0500

    Final! Added SnackBar and much more~ Passed edittext to second screen~ Added log when app is moved to background~ Added support for french~ Minor fixes to match desired assignment layout~ Improvements to program~

--- DylanLab11/src/main/java/Dylan/Ashton/n01442206/AshtonActivity.java
+++ DylanLab11/src/main/java/Dylan/Ashton/n01442206/AshtonActivity.java
@@ -3,13 +3,20 @@ package Dylan.Ashton.n01442206;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-//Dylan Ashton n01442206
+//Dylan Ashton n01442206 RNA
 public class AshtonActivity extends AppCompatActivity {
 
+    TextView Edit;
+    String Txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ashton);
+
+        Edit=findViewById(R.id.DylanTextView2);
+        Txt=getIntent().getExtras().getString("Value");
+        Edit.setText(Txt);
     }
 }
--- DylanLab11/src/main/java/Dylan/Ashton/n01442206/DylanActivity.java
+++ DylanLab11/src/main/java/Dylan/Ashton/n01442206/DylanActivity.java
@@ -5,49 +5,66 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 //Dylan Ashton n01442206 RNA
 public class DylanActivity extends AppCompatActivity {
     private Button DylanButton1;
     private Button DylanButton2;
+    private CoordinatorLayout coordinatorlayout;
+    private EditText et;
+    private String Txt;
+    private String tag="Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dylan);
 
-        DylanButton1 = (Button) findViewById(R.id.DylanButton1);
-        DylanButton2 = (Button) findViewById(R.id.DylanButton2);
-        DylanButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                OpenNewScreen();
-            }
+
+        et=findViewById(R.id.DylanEditText);
+
+        DylanButton1 = findViewById(R.id.DylanButton1);    //Button 1
+        DylanButton1.setOnClickListener(view -> {
+            DisplayToast();
+            GoToWebPage("https://www.youtube.com/");
         });
-        DylanButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                displayToast();
-                goToWebPage("https://www.youtube.com/");
-            }
-        });
+
+        coordinatorlayout = findViewById(R.id.DylanCoordinatorLayout); //Button 2 with SnackBar
+        DylanButton2 = findViewById(R.id.DylanButton2);
+        DylanButton2.setOnClickListener(view -> Showsnackbar());
+
     }
 
-    public void OpenNewScreen() {
-        Intent intent = new Intent(this, AshtonActivity.class);
-        startActivity(intent);
+    public void DisplayToast() {
+        Toast.makeText(DylanActivity.this, "Dylan Ashton , youtube", Toast.LENGTH_SHORT).show(); //unable to pass string.xml values
     }
 
-    public void displayToast() {
-        Toast.makeText(DylanActivity.this, "Dylan Ashton , youtube", Toast.LENGTH_SHORT).show();
-    }
-
-    public void goToWebPage(String yourUrl) {
+    public void GoToWebPage(String yourUrl) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(yourUrl));
         startActivity(intent);
+    }
+    public void Showsnackbar() {
+        Snackbar snackbar = Snackbar.make(coordinatorlayout, "Dylan Ashton Second screen", Snackbar.LENGTH_INDEFINITE) //unable to pass string.xml values
+                .setAction("Next Screen", v -> Pass());
+        snackbar.show();
+    }
+    public void Pass(){
+        Intent i=new Intent(DylanActivity.this,AshtonActivity.class);
+        Txt=et.getText().toString();
+        i.putExtra("Value",Txt); //unable to pass string.xml values
+        startActivity(i);
+    }
+    public void onStop(){
+        super.onStop();
+        Log.d(tag,"Dylan Ashton n01442206"); //unable to pass string.xml values
     }
 }
